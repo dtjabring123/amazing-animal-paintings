30809f5f38b446d9ee8f79cc58cd25455424e55e
Dylan Tjabring <tjabringd@aforbes.com>
2026-03-01 14:17:42 +0200

    Search Functionality

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule, MatSnackBarModule],
+  imports: [CommonModule, MatCardModule, MatSnackBarModule, MatInputModule],
 })
 export class ProductModule {}

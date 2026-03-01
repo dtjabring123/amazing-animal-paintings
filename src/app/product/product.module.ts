264a46c1e7a382921a832fe36f7be592aaa91d34
Dylan Tjabring <tjabringd@aforbes.com>
2026-03-01 14:27:34 +0200

    Sort functionality

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,9 +4,16 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule, MatSnackBarModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
 })
 export class ProductModule {}
